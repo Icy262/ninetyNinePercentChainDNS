fc73522e8c1fd2d086176385035cb2450b3fbe5a
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-01 17:02:12 -0400

    Fixed KeepAlive sendout

--- KeepAlive.java
+++ KeepAlive.java
@@ -1,11 +1,24 @@
 import java.net.Socket;
 import java.lang.Thread;
+import java.net.SocketTimeoutException;
 
 class KeepAlive extends Thread {
-	public void run(String ip) throws SocketTimeoutException {
+	private String ip;
+	public void run() {
 		int port=9937;
-		Socket socket=new Socket(ip, port);
-		socket.setSoTimeout(10000);
-		socket.close();
+		try {
+			Socket socket=new Socket(ip, port);
+			socket.setSoTimeout(10000);
+			socket.close();
+		} catch(Exception e) {
+			if(e instanceof SocketTimeoutException) {
+				//REMOVE IP FROM LIST
+			} else {
+				System.out.println();
+			}
+		}
+	}
+	public KeepAlive(String ip) {
+		this.ip=ip;
 	}
 }
