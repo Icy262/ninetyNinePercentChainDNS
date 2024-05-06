86cd352c1ad9d8bcef85156779ebedae80d67fdd
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-06 10:37:14 -0400

    Created DNSResponse thread
    Thread to automatically respond to node requests for ip addresses.

---
+++ DNSResponse.java
@@ -0,0 +1,24 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.lang.Thread;
+import java.io.ObjectOutputStream;
+
+class DNSResponse extends Thread {
+	public void run() {
+		int port=9937;
+		try {
+			ServerSocket socket=new ServerSocket(port);
+			while(true) {
+				Socket connection=socket.accept();	
+				ObjectOutputStream write=new ObjectOutputStream(connection.getOutputStream());
+				for(int i=0; i<10; i++) {
+					write.writeObject(NodeIP.getNextIP());
+				}
+				write.close();
+				socket.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
