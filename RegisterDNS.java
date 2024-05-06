c731df7900ab7b085209d0691ab539af9bd08b70
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-06 10:38:35 -0400

    Added RegisterDNS
    RegisterDNS allows nodes to add their ip to the list.

---
+++ RegisterDNS.java
@@ -0,0 +1,22 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.lang.Thread;
+import java.io.ObjectInputStream;
+
+class DNSResponse extends Thread {
+	public void run() {
+		int port=9937;
+		try {
+			ServerSocket socket=new ServerSocket(port);
+			while(true) {
+				Socket connection=socket.accept();	
+				ObjectInputStream read=new ObjectInputStream(connection.getInputStream());
+				NodeIP.addIP((String) read.readObject());
+				read.close();
+				socket.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
