e5973fb0f76522ec62538c6739e68e08dbae6c0f
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-08 08:16:23 -0400

    DNSResponse and RegisterDNS now run in threads

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		new DNSResponse().start();
+		new RegisterDNS().start();
+		try {
+			while(true) {
+				for(int i=0; i<NodeIP.getSize(); i++) {
+					new KeepAlive(NodeIP.getIP(i)).start();
+				}
+				Thread.sleep(60000);
+			}
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+}
