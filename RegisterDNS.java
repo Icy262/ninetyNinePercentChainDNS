a41dbafb42d9129d14002e4d6b20e5bea4ad5827
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-08 08:31:43 -0400

    Changed RegisterDNS from port 9938 to 9941

--- RegisterDNS.java
+++ RegisterDNS.java
@@ -5,7 +5,7 @@ import java.io.ObjectInputStream;
 
 class RegisterDNS extends Thread {
 	public void run() {
-		int port=9938;
+		int port=9941;
 		try {
 			ServerSocket socket=new ServerSocket(port);
 			while(true) {
