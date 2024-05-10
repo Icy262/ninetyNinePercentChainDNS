e0375a060a50b00852747836dece046248bd5a27
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-10 13:30:17 -0400

    Now can handle multiple requests
    Moved port listening to handler and handler creates a new thread on connection.

--- RegisterDNS.java
+++ RegisterDNS.java
@@ -1,22 +1,19 @@
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.lang.Thread;
 import java.io.ObjectInputStream;
 
 class RegisterDNS extends Thread {
+	Socket connection;
 	public void run() {
-		int port=9941;
 		try {
-			ServerSocket socket=new ServerSocket(port);
-			while(true) {
-				Socket connection=socket.accept();	
-				ObjectInputStream read=new ObjectInputStream(connection.getInputStream());
-				NodeIP.addIP((String) read.readObject());
-				read.close();
-				socket.close();
-			}
-		} catch (Exception e) {
+			ObjectInputStream read=new ObjectInputStream(connection.getInputStream());
+			NodeIP.addIP((String) read.readObject());
+			read.close();
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public RegisterDNS(Socket connection) {
+		this.connection=connection;
 	}
 }
