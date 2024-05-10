4004c7402422be8fd6b77de3d73de7014f9438e8
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-10 13:29:28 -0400

    Moved code to KeepAliveHandler

--- Main.java
+++ Main.java
@@ -1,16 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		new DNSResponse().start();
-		new RegisterDNS().start();
-		try {
-			while(true) {
-				for(int i=0; i<NodeIP.getSize(); i++) {
-					new KeepAlive(NodeIP.getIP(i)).start();
-				}
-				Thread.sleep(300000);
-			}
-		} catch(Exception e) {
-			System.out.println(e);
-		}
+		new DNSResponseHandler().start();
+		new RegisterDNSHandler().start();
+		new KeepAliveHandler().start();
 	}
 }
