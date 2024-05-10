997cc5312bf9dc18680688bc05770ce193057b67
Icy262 <114495528+Icy262@users.noreply.github.com>
2024-05-10 13:28:26 -0400

    Added multithreading
    Port listening is now handled by DNSResponseHandler.

--- DNSResponse.java
+++ DNSResponse.java
@@ -1,24 +1,22 @@
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.lang.Thread;
 import java.io.ObjectOutputStream;
 
 class DNSResponse extends Thread {
+	Socket connection;
 	public void run() {
-		int port=9940;
 		try {
-			ServerSocket socket=new ServerSocket(port);
-			while(true) {
-				Socket connection=socket.accept();	
-				ObjectOutputStream write=new ObjectOutputStream(connection.getOutputStream());
-				for(int i=0; i<10; i++) {
-					write.writeObject(NodeIP.getNextIP());
-				}
-				write.close();
-				socket.close();
+			ObjectOutputStream write=new ObjectOutputStream(connection.getOutputStream());
+			for(int i=0; i<10; i++) {
+				write.writeObject(NodeIP.getNextIP());
 			}
-		} catch (Exception e) {
+			write.close();
+			connection.close();
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	public DNSResponse(Socket connection) {
+		this.connection=connection;
 	}
 }
